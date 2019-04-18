9894bc6d55b82d40b4c24ea358f074680ecc5d74
Gwyd10n <gwydion.oostvogel@gmail.com>
2019-04-18 02:28:35 +0200

    plots

--- Homework/Week_2/application.py
+++ Homework/Week_2/application.py
@@ -7,6 +7,7 @@ Preprocessing data obtained from csv file.
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import json
 
 
 def read_csv(path):
@@ -53,23 +54,64 @@ def centr_tend(df, column_name):
     :param column_name: string
     :return: none
     """
-    mean = df['GDP ($ per capita) dollars'].mean()
-    median = df['GDP ($ per capita) dollars'].median()
-    mode = df['GDP ($ per capita) dollars'].mode()[0]
-    std_dev = df['GDP ($ per capita) dollars'].std()
+    mean = df[column_name].mean()
+    median = df[column_name].median()
+    mode = df[column_name].mode()[0]
+    std_dev = df[column_name].std()
 
-    print(f"Column: {column_name}\nMean: {mean}\nMedian: {median}\nMode: {mode}\nStandart deviation: {std_dev}")
+    print(f"Central tendency of {column_name}\nMean: {mean}\nMedian: {median}\nMode: {mode}\nStandart deviation: {std_dev}\n")
 
 
 def plot_hist(df, column_name):
+    """
+    Plot histogram of specified column.
+    :param df: object
+    :param column_name: string
+    :return: none
+    """
     GDP_list = [value for value in df[column_name].tolist() if value <= 50000]
     plt.hist(GDP_list, normed=True, bins=48)
     plt.show()
 
 
+def five_nr_summ(df, column_name):
+    """
+    Prints minimum, first quarter, median, third quarter and maximum of column in data frame.
+    :param df: object
+    :param column_name: string
+    :return: none
+    """
+    min = df[column_name].min()
+    fi_quart = df[column_name].quantile(q=0.25)
+    median = df[column_name].median()
+    th_quart = df[column_name].quantile(q=0.75)
+    max = df[column_name].max()
+
+    print(f"Five number summary of {column_name}\nMinimum: {min}\nFirst quartile: {fi_quart}\nMedian: {median}\n"
+          f"Third quartile: {th_quart}\nMaximum: {max}\n")
+
+
+def plot_box(df, column_name):
+    """
+    Plot boxplot of specified column.
+    :param df: object
+    :param column_name: string
+    :return: none
+    """
+    df.boxplot(column=column_name)
+    plt.show()
+
+
+def save_json():
+    pass
+
+
+
 if __name__ == "__main__":
     data = clean_data(read_csv('input.csv'))
     centr_tend(data, 'GDP ($ per capita) dollars')
+    five_nr_summ(data, 'Infant mortality (per 1000 births)')
+    save_json()
     plot_hist(data, 'GDP ($ per capita) dollars')
+    plot_box(data, 'Infant mortality (per 1000 births)')
 
-    # print(data)

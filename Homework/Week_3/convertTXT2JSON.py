61225eff0c395d287b686e5d830a601cbed45608
Gwyd10n <gwydion.oostvogel@gmail.com>
2019-04-30 16:07:56 +0200

    write to json

---
+++ Homework/Week_3/convertTXT2JSON.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# Name: Gwydion Oostvogel
+# Student number: 12578002
+
+import sys
+import re
+import json
+
+
+def read_txt(infile):
+    data = {}
+    with open(infile, 'r') as file:
+        for line in file:
+            if '#' in line:
+                continue
+            string_list = re.sub('[\n\t ]', '', line).split(',')
+            try:
+                data_list = [int(val) for val in string_list]
+                data[data_list[1]] = {'TG': data_list[2], 'PG': data_list[3]}
+            except ValueError:
+                pass
+    file.close()
+    print(data)
+    return data
+
+
+def write_json(dict):
+    with open(sys.argv[1] + '.json', 'w') as outfile:
+        json.dump(dict, outfile)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Error: incorrect number of arguments")
+        sys.exit(1)
+    write_json(read_txt(sys.argv[1]+'.txt'))

d5e952d7e55445a40d4b7d91b2c0e5b9af02120f
Gwyd10n <gwydion.oostvogel@gmail.com>
2019-05-29 20:29:26 +0200

    iets

--- Homework/Week_6/linked_views.js
+++ Homework/Week_6/linked_views.js
@@ -5,9 +5,6 @@
 
 function worldmap(HPIdata) {
 
-    // console.log(HPIdata);
-
-
     let map = new Datamap({
         element: document.getElementById("container"),
         projection: 'mercator',
@@ -15,19 +12,15 @@ function worldmap(HPIdata) {
         data: HPIdata,
         geographyConfig: {
             popupTemplate: function (geography) {
-                for (let idx = 0; idx < HPIdata.length; idx++) {
-                    console.log("hey");
-                    if (HPIdata[idx].country == geography.properties.name){
-                        console.log(HPIdata[country])
-                        return '<div class="hoverinfo">' + geography.properties.name + ' ' + HPIdata[idx];
-                    }
-                }
-                return '<div class="hoverinfo">' + geography.properties.name;
+                return '<div class="hoverinfo">' + geography.properties.name + ' HPI: ' + HPIdata[geography.properties.name]['happy_planet_index'];
             }
         }
     });
-    map.updateChoropleth({USA: "#FF0000"})
+    map.updateChoropleth({LOW: "#FF0000"}, {MEDIUM: "#777700"}, {HIGH: "#00FF00"});
+    for (let country in HPIdata) {
+        console.log()
     }
+}
 
 function loadJSON(path) {
     d3v5.json(path).then(function (d) {

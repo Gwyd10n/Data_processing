98d076e837fff15712a48990b913f49c03cc6c34
Gwyd10n <gwydion.oostvogel@gmail.com>
2019-05-30 03:10:44 +0200

    d3 link

--- Homework/Week_6/linked_views.js
+++ Homework/Week_6/linked_views.js
@@ -5,20 +5,46 @@
 
 function worldmap(HPIdata) {
 
+    // Get array with all topo objects
+    let cc = Datamap.prototype.worldTopo.objects.world.geometries;
+    let ccdict = {};
+    // Add country and country code to dict
+    for (let i = 0, l = cc.length; i < l; i++) {
+        ccdict[cc[i].properties.name] = cc[i].id
+    }
+
+    // Function to get HPI value for country
+    let getHPI = function (country) {
+        return HPIdata[country]
+    };
+
+    getHPI('Belgium')
+
+        let color = d3v3.scale.linear().domain([1,50])
+      .interpolate(d3v3.interpolateHcl)
+      .range([d3v3.rgb("#007AFF"), d3v3.rgb('#FFF500')]);
+
+    // let color = d3v5.scale().domain([1,50]).range(["#FF0000", "#00FF00"]);
+    // let color = d3v5.interpolateRgb("#FF0000", "#00FF00");
+    console.log(color(25))
+
     let map = new Datamap({
         element: document.getElementById("container"),
         projection: 'mercator',
-        fills: {defaultFill: "#228B22"},
         data: HPIdata,
+        fills: {defaultFill: "#3f3f3f"},
         geographyConfig: {
             popupTemplate: function (geography) {
-                return '<div class="hoverinfo">' + geography.properties.name + ' HPI: ' + HPIdata[geography.properties.name]['happy_planet_index'];
+                return '<div class="hoverinfo">' + geography.properties.name + ' HPI: ' +
+                    HPIdata[geography.properties.name]['happy_planet_index'] + '</div>';
             }
         }
     });
-    map.updateChoropleth({LOW: "#FF0000"}, {MEDIUM: "#777700"}, {HIGH: "#00FF00"});
+
+
+
     for (let country in HPIdata) {
-        console.log(HPIdata[country])
+        map.updateChoropleth({USA: color(getHPI(country))});
     }
 }
 
